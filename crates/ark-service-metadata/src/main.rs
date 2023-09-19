2cb6d5ff2ca9ed42eb3d9a73b9b7665e32e34e9e
Rémi Roycourt <remiroycourt@gmail.com>
2023-09-19 20:02:11 +0200

    feat(metadata): create metadata manager

--- crates/ark-service-metadata/src/main.rs
+++ crates/ark-service-metadata/src/main.rs
@@ -1,5 +1,24 @@
 use anyhow::Result;
+use ark_rs::starknet::client::{StarknetClient, StarknetClientHttp};
+use ark_rs::{
+    nft_metadata::{file_manager::LocalFileManager, metadata_manager::MetadataManager},
+    nft_storage::DefaultStorage,
+};
+use dotenv::dotenv;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv().ok();
+
+    let rpc_url = std::env::var("RPC_PROVIDER").expect("RPC_PROVIDER must be set");
+
+    let storage_manager = DefaultStorage::new();
+    let starknet_client =
+        StarknetClientHttp::new(&rpc_url).expect("Failed to create Starknet client");
+    let file_manager = LocalFileManager::new();
+    let metadata_manager = MetadataManager::new(&storage_manager, &starknet_client, &file_manager);
+
+    // metadata_manager.fetch_token_image(url, name, cache_image);
+
+    Ok(())
 }
