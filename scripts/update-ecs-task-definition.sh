21b07d53f397ff6ce2f2c389b6df1ea303d3f319
Rémi Roycourt <remiroycourt@gmail.com>
2023-09-29 22:14:12 +0200

    ci: add release workflow

---
+++ scripts/update-ecs-task-definition.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "❌ Usage: $0 <TASK_DEFINITION_NAME> <NEW_ARK_INDEXER_VERSION>"
+    exit 1
+fi
+
+TASK_DEFINITION_NAME=$1
+NEW_ARK_INDEXER_VERSION=$2
+
+TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition $TASK_DEFINITION_NAME --query "taskDefinition")
+CURRENT_ARK_INDEXER_VERSION=$(echo $TASK_DEFINITION | jq -r '.containerDefinitions[0].environment[] | select(.name=="ARK_INDEXER_VERSION") | .value')
+
+if [ "$CURRENT_ARK_INDEXER_VERSION" = "$NEW_ARK_INDEXER_VERSION" ]; then
+    echo "✅ The ARK_INDEXER_VERSION is already set to $NEW_ARK_INDEXER_VERSION. No action needed."
+    exit 0
+else
+    echo "🔄 Current ARK_INDEXER_VERSION is $CURRENT_ARK_INDEXER_VERSION. Updating to $NEW_ARK_INDEXER_VERSION..."
+   
+    # Modify ARK_INDEXER_VERSION in the task definition
+    NEW_TASK_DEFINITION=$(echo $TASK_DEFINITION | jq --arg NEW_ARK_INDEXER_VERSION "$NEW_ARK_INDEXER_VERSION" '
+      .containerDefinitions[0].environment |= map(
+        if .name == "ARK_INDEXER_VERSION" then
+          .value = $NEW_ARK_INDEXER_VERSION
+        else
+          .
+        end
+      )
+    ')
+
+    echo $NEW_TASK_DEFINITION
+
+    NEW_TASK_DEF_REGISTER_RESULT=$(aws ecs register-task-definition --family "$TASK_DEFINITION_NAME" --cli-input-json "$NEW_TASK_DEFINITION")
+
+    if [ $? -eq 0 ]; then
+        echo "✨ Successfully registered new task definition with ARK_INDEXER_VERSION=$NEW_ARK_INDEXER_VERSION"
+    else
+        echo "❌ Failed to register new task definition"
+        exit 1
+    fi
+fi
+
