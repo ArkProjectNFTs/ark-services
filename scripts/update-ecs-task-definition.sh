f894bcfe355f93d4506396219980c8a401c2d8ad
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2023-10-04 00:44:08 +0200

    ci: update task def (#16)
    * ci: update task def
    
    * ci: update task def

--- scripts/update-ecs-task-definition.sh
+++ scripts/update-ecs-task-definition.sh
@@ -8,7 +8,9 @@ fi
 TASK_DEFINITION_NAME=$1
 NEW_ARK_INDEXER_VERSION=$2
 
-TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition $TASK_DEFINITION_NAME --query "taskDefinition")
+# Get the task definition excluding taskDefinitionArn and revision
+TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition $TASK_DEFINITION_NAME --query "taskDefinition | {family: family, containerDefinitions: containerDefinitions, volumes: volumes, placementConstraints: placementConstraints, networkMode: networkMode, executionRoleArn: executionRoleArn, memory: memory, cpu: cpu, requiresCompatibilities: requiresCompatibilities}")
+
 CURRENT_ARK_INDEXER_VERSION=$(echo $TASK_DEFINITION | jq -r '.containerDefinitions[0].environment[] | select(.name=="ARK_INDEXER_VERSION") | .value')
 
 if [ "$CURRENT_ARK_INDEXER_VERSION" = "$NEW_ARK_INDEXER_VERSION" ]; then
@@ -30,13 +32,12 @@ else
 
     echo $NEW_TASK_DEFINITION
 
-    NEW_TASK_DEF_REGISTER_RESULT=$(aws ecs register-task-definition --family "$TASK_DEFINITION_NAME" --cli-input-json "$NEW_TASK_DEFINITION")
+    # NEW_TASK_DEF_REGISTER_RESULT=$(aws ecs register-task-definition --cli-input-json "$NEW_TASK_DEFINITION")
 
-    if [ $? -eq 0 ]; then
-        echo "✨ Successfully registered new task definition with ARK_INDEXER_VERSION=$NEW_ARK_INDEXER_VERSION"
-    else
-        echo "❌ Failed to register new task definition"
-        exit 1
-    fi
+    # if [ $? -eq 0 ]; then
+    #     echo "✨ Successfully registered new task definition with ARK_INDEXER_VERSION=$NEW_ARK_INDEXER_VERSION"
+    # else
+    #     echo "❌ Failed to register new task definition"
+    #     exit 1
+    # fi
 fi
-
