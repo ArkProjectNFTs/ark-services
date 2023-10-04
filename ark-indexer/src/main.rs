cd2142ba536226086323af504e43292e6041c48b
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2023-10-04 15:44:03 +0200

    fix(ark-indexer): use tracing & fix indexer version typing issue (#25)
    * fix(ark-indexer): use tracing & fix indexer version typing issue
    
    * fix: cargo fmt
    
    * fix: clippy issue
    
    * fix(deps): arkproject dependency
    
    * fix(deps): arkproject dependency

--- ark-indexer/src/main.rs
+++ ark-indexer/src/main.rs
@@ -10,15 +10,19 @@ use arkproject::{
 };
 
 use dotenv::dotenv;
-use log::info;
 use pontos_observer::PontosObserver;
 use regex::Regex;
 use starknet::core::types::{BlockId, BlockTag};
 use std::{env, sync::Arc};
+use tracing::{debug, info, trace};
+use tracing::{span, Level};
+use tracing_subscriber::{fmt, layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
+
+    init_tracing();
 
     let is_head_of_chain = env::var("HEAD_OF_CHAIN").is_ok();
 
@@ -46,6 +50,16 @@ async fn main() -> Result<()> {
     let indexer_version = env::var("INDEXER_VERSION").expect("INDEXER_VERSION must be set");
     let indexer_identifier = get_task_id();
 
+    info!(
+        "🏁 Starting Indexer. Version={}, Identifier={}",
+        indexer_version, indexer_identifier
+    );
+
+    debug!(
+        "from_block={:?}, to_block={:?}, head_of_the_chain={}, rpc_url={}, table_name={}, force_mode={}, indexer_version={}, indexer_identifier={}",
+       from_block, to_block, is_head_of_chain, rpc_url, table_name, force_mode, indexer_version, indexer_identifier
+    );
+
     let dynamo_storage = Arc::new(DynamoStorage::new(table_name.clone()).await);
     let starknet_client = Arc::new(StarknetClientHttp::new(rpc_url.as_str())?);
 
@@ -66,10 +80,10 @@ async fn main() -> Result<()> {
     );
 
     if is_head_of_chain {
-        log::trace!("Syncing Pontos at head of the chain");
+        trace!("Syncing Pontos at head of the chain");
         pontos_task.index_pending().await?;
     } else {
-        log::trace!(
+        trace!(
             "Syncing Pontos for block range: {:?} - {:?}",
             from_block,
             to_block
@@ -82,15 +96,41 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-pub fn get_task_id() -> String {
-    let container_metadata_uri = env::var("ECS_CONTAINER_METADATA_URI").unwrap_or("".to_string());
+fn get_task_id() -> String {
+    match env::var("ECS_CONTAINER_METADATA_URI") {
+        Ok(container_metadata_uri) => {
+            let pattern = Regex::new(r"/v3/([a-f0-9]{32})-").unwrap();
+            let task_id = pattern
+                .captures(container_metadata_uri.as_str())
+                .and_then(|cap| cap.get(1).map(|m| m.as_str()))
+                .expect("Can't parse task id from ECS_CONTAINER_METADATA_URI");
 
-    let pattern = Regex::new(r"/v3/([a-f0-9]{32})-").unwrap();
-    let task_id = pattern
-        .captures(container_metadata_uri.as_str())
-        .and_then(|cap| cap.get(1).map(|m| m.as_str()))
-        .unwrap_or("");
+            task_id.to_string()
+        }
+        Err(_) => {
+            if env::var("HEAD_OF_CHAIN").is_ok() {
+                String::from("LATEST")
+            } else {
+                String::from("LOCALHOST")
+            }
+        }
+    }
+}
 
-    info!("Indexer Task ID: {:?}", task_id);
-    task_id.to_string()
+fn init_tracing() {
+    // Initialize the LogTracer to convert `log` records to `tracing` events
+    tracing_log::LogTracer::init().expect("Setting log tracer failed.");
+
+    // Create the layers
+    let env_filter = EnvFilter::from_default_env();
+    let fmt_layer = fmt::layer();
+
+    // Combine layers and set as global default
+    let subscriber = Registry::default().with(env_filter).with(fmt_layer);
+
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Setting default subscriber failed.");
+
+    let main_span = span!(Level::TRACE, "main");
+    let _main_guard = main_span.enter();
 }
