d2b0d701c34c53ace140ba42c9f7626868a06720
kwiss <mr.dumont.christophe@gmail.com>
2023-10-18 01:20:23 +0200

    fix(cdk-naming): fix wrong naming

--- infrastructure/cdk/bin/cdk.ts
+++ infrastructure/cdk/bin/cdk.ts
@@ -67,7 +67,9 @@ new ArkStack(app, stackName, {
   },
   branch: baseBranch,
   stages: stages,
-  isPullRequest: isPullRequest ? true : false,
+  isRelease: isRelease,
+  isPullRequest: isPullRequest,
+  prNumber: prNumber,
   description:
     "This stack provisions the infrastructure for the Ark Project, which includes API endpoints for contract management and token events. It integrates with DynamoDB for data storage and provides Lambda functions for specific API operations. The stack is designed to be environment-agnostic and can be deployed to any AWS region.",
   /* If you don't specify 'env', this stack will be environment-agnostic.
