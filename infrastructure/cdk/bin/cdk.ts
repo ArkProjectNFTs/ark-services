4b0440caf03b573eaacfc7c17b5901c1e205b095
christophe <mr.dumont.christophe@gmail.com>
2023-10-17 10:57:15 +0200

    ci(push-deploy): fix boolean (#46)

--- infrastructure/cdk/bin/cdk.ts
+++ infrastructure/cdk/bin/cdk.ts
@@ -34,7 +34,7 @@ new ArkStack(app, stackName, {
   },
   branch: branch,
   stages: stages,
-  isPullRequest: isPullRequest,
+  isPullRequest: isPullRequest === 'true' ? true : false,
   description:
     "This stack provisions the infrastructure for the Ark Project, which includes API endpoints for contract management and token events. It integrates with DynamoDB for data storage and provides Lambda functions for specific API operations. The stack is designed to be environment-agnostic and can be deployed to any AWS region.",
   /* If you don't specify 'env', this stack will be environment-agnostic.
