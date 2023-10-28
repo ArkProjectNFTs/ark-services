24df63dd5e52ca64d0c55a886e7abc71f599c42f
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2023-10-28 18:27:01 +0200

    feat(ark-admin): add log level (#73)

--- ark-admin/src/components/dashboard/DataTable.tsx
+++ ark-admin/src/components/dashboard/DataTable.tsx
@@ -25,6 +25,8 @@ import {
   TableRow,
 } from "~/components/ui/table";
 import { DataTablePagination } from "./DataTablePagination";
+import { DataTableToolbar } from "./DataTableToolbar";
+import { DataTableViewOptions } from "./DataTableViewOptions";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -67,7 +69,7 @@ export function DataTable<TData, TValue>({
 
   return (
     <div className="space-y-4">
-      {/* <DataTableToolbar table={table} /> */}
+      <DataTableToolbar table={table} />
       <div className="rounded-md border">
         <Table>
           <TableHeader>
