1f4021099320e5f7cef6caca146a439d1f753770
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2023-11-01 16:28:00 +0100

    fix(ark-admin): all blocks pagination (#78)
    * fix(ark-admin): all blocks pagination
    
    * fix(ark-admin): fix range task popover button
    
    * perf(ark-admin): make fetchblocks 2x faster
    
    ---------
    
    Co-authored-by: Paul Launay <launay.p@gmail.com>

--- ark-admin/src/lib/fetchBlocks.ts
+++ ark-admin/src/lib/fetchBlocks.ts
@@ -1,58 +1,123 @@
+import type {
+  AttributeValue,
+  QueryCommandOutput,
+} from "@aws-sdk/client-dynamodb";
+
 import { db } from "~/server/dynamodb";
 import { type Network } from "~/types";
 import { type Range } from "./range";
 
+/**
+ * Fetches blocks from the database, calculates ranges and returns them.
+ *
+ * @param {Network} network - Network type parameter.
+ * @param {number} latest - Latest block number.
+ *
+ * @returns {Object} - Returns ranges, rangeSize, and count.
+ */
 export async function fetchBlocks(network: Network, latest: number) {
-  const dynamoResult = await db.query({
-    TableName: `ark_project_${network}`,
-    IndexName: "GSI1PK-GSI1SK-index",
-    KeyConditionExpression: "#GSI1PK = :GSI1PK",
-    ExpressionAttributeNames: {
-      "#GSI1PK": "GSI1PK",
-      "#status": "Data.M.Status",
-    },
-    ExpressionAttributeValues: {
-      ":GSI1PK": { S: "BLOCK" },
-      ":noneValue": { S: "NONE" },
-    },
-    FilterExpression: "#status <> :noneValue",
-    ProjectionExpression: "PK",
-  });
-
-  const count = latest - (dynamoResult.Items?.length ?? 0);
+  const items: Record<string, AttributeValue>[] =
+    await fetchAllDynamoItems(network);
+  const count = latest - items.length;
   const rangeCount = 120;
   const rangeSize = Math.ceil(latest / rangeCount);
-  const ranges: Range[] = [];
 
-  // Initialize the ranges with empty blocks arrays
-  for (let i = 0; i < rangeCount; i++) {
+  const ranges: Range[] = createEmptyRanges(latest, rangeCount, rangeSize);
+  populateRangesWithBlocks(ranges, items, rangeSize, latest);
+
+  return { ranges, rangeSize, count };
+}
+
+/**
+ * Fetch all items from DynamoDB for a given network.
+ *
+ * @param {Network} network - Network type parameter.
+ *
+ * @returns {Promise<Record<string, AttributeValue>[]>} - Returns all fetched items.
+ */
+async function fetchAllDynamoItems(
+  network: Network,
+): Promise<Record<string, AttributeValue>[]> {
+  let lastEvaluatedKey: Record<string, AttributeValue> | undefined = undefined;
+  const items: Record<string, AttributeValue>[] = [];
+
+  do {
+    const result: QueryCommandOutput = await db.query({
+      TableName: `ark_project_${network}`,
+      IndexName: "GSI1PK-GSI1SK-index",
+      KeyConditionExpression: "#GSI1PK = :GSI1PK",
+      ExpressionAttributeNames: {
+        "#GSI1PK": "GSI1PK",
+        "#status": "Data.M.Status",
+      },
+      ExpressionAttributeValues: {
+        ":GSI1PK": { S: "BLOCK" },
+        ":noneValue": { S: "NONE" },
+      },
+      FilterExpression: "#status <> :noneValue",
+      ProjectionExpression: "PK",
+      ExclusiveStartKey: lastEvaluatedKey,
+    });
+
+    items.push(...(result.Items ?? []));
+    lastEvaluatedKey = result.LastEvaluatedKey;
+  } while (lastEvaluatedKey);
+
+  return items;
+}
+
+/**
+ * Creates a list of empty ranges based on provided parameters.
+ *
+ * @param {number} latest - Latest block number.
+ * @param {number} rangeCount - Total count of ranges.
+ * @param {number} rangeSize - Size of each range.
+ *
+ * @returns {Range[]} - Returns an array of empty ranges.
+ */
+function createEmptyRanges(
+  latest: number,
+  rangeCount: number,
+  rangeSize: number,
+): Range[] {
+  return Array.from({ length: rangeCount }, (_, i) => {
     const start = i * rangeSize;
     const end = i !== rangeCount - 1 ? (i + 1) * rangeSize - 1 : latest;
-    ranges.push({ start, end, blocks: [] });
-  }
+    return { start, end, blocks: [] };
+  });
+}
 
+/**
+ * Populates the ranges with blocks from the list of all items.
+ *
+ * @param {Range[]} ranges - Array of ranges.
+ * @param {Record<string, AttributeValue>[]} allItems - All fetched items.
+ * @param {number} rangeSize - Size of each range.
+ * @param {number} latest - Latest block number.
+ */
+function populateRangesWithBlocks(
+  ranges: Range[],
+  items: Record<string, AttributeValue>[],
+  rangeSize: number,
+  latest: number,
+) {
   let nextExpectedBlock = 0;
 
-  for (const item of dynamoResult.Items ?? []) {
+  for (const item of items) {
     const block = +(item.PK?.S?.split("#")[1] ?? 0);
 
-    // Fill in missing blocks until the current block
     while (nextExpectedBlock < block) {
       const rangeIndex = Math.floor(nextExpectedBlock / rangeSize);
       ranges[rangeIndex]?.blocks.push(nextExpectedBlock);
       nextExpectedBlock++;
     }
 
-    // Set the next expected block
     nextExpectedBlock = block + 1;
   }
 
-  // Fill in missing blocks for the remainder of the range
   while (nextExpectedBlock <= latest) {
     const rangeIndex = Math.floor(nextExpectedBlock / rangeSize);
     ranges[rangeIndex]?.blocks.push(nextExpectedBlock);
     nextExpectedBlock++;
   }
-
-  return { ranges, rangeSize, count };
 }
