4e9cb07f78a5f246c167ef3d0fc2d1e0cbb49dfd
Paul Launay <launay.p@gmail.com>
2023-11-01 18:30:59 +0100

    feat(ark-admin): improve block range infos

--- ark-admin/src/components/dashboard/BlockRange.tsx
+++ ark-admin/src/components/dashboard/BlockRange.tsx
@@ -13,8 +13,11 @@ type BlockRangeProps = {
   blocks: number[];
 };
 
+const GRADIENT_OFFSET = 30;
+
 export default function BlockRange({ start, end, blocks }: BlockRangeProps) {
   const { setValues } = useTaskForm();
+  const percent = 100 - Math.round((blocks.length / (end - start + 1)) * 100);
 
   const handleAddtask = (blocks: number[]) => {
     setValues({
@@ -28,22 +31,26 @@ export default function BlockRange({ start, end, blocks }: BlockRangeProps) {
     <Popover>
       <PopoverTrigger className="w-full cursor-default">
         <div
-          className={cn(
-            "flex h-6 w-full select-none justify-between bg-green-500 p-1 text-xs text-white",
-            blocks.length && "bg-red-500 ",
-          )}
-        />
+          className="h-6 w-full select-none items-end p-1 text-right text-xs text-white"
+          style={{
+            background: `linear-gradient(90deg, #10B981 ${
+              percent === 100 ? 100 : percent - GRADIENT_OFFSET
+            }%, #EF4444 ${percent ? percent + GRADIENT_OFFSET : 0}%)`,
+          }}
+        >
+          {blocks.length ? blocks.length : ""}
+        </div>
       </PopoverTrigger>
       <PopoverContent className="w-48 p-2">
         <div className="">
-          <div className="mb-4 flex items-center text-xs">
+          <div className={cn("mb-4 flex items-center rounded-sm p-1 text-xs")}>
             <div>#{start.toLocaleString()}</div>
             <MoveRight size={14} className="flex-grow" />
             <div>#{end.toLocaleString()}</div>
           </div>
           <div className="">
             <div className="text-xs text-muted-foreground">Blocks</div>
-            <div className="text-2xl font-semibold">{end - start}</div>
+            <div className="text-2xl font-semibold">{end - start + 1}</div>
           </div>
           <div className="">
             <div className="text-xs text-muted-foreground">
