e938cfed80b57a61ac49e71bc2044444a1bc4cb0
glihm <dev@glihm.net>
2023-12-17 14:29:31 -0600

    fix: update starknet version and bump ark-project release

--- ark-sqlx/src/providers/orderbook/order.rs
+++ ark-sqlx/src/providers/orderbook/order.rs
@@ -13,7 +13,7 @@ pub enum OrderStatus {
 }
 
 impl fmt::Display for OrderStatus {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
             "{}",
