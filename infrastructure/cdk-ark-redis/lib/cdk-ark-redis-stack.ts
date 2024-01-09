4cb3fcbf1af5067d72e0a0df9a4ede9dcae56ae9
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2024-01-09 18:17:26 +0100

    chore: update ark-redis deployment workflow (#146)
    * chore: deploy redis on aws
    
    * chore: fix ark-redis

--- infrastructure/cdk-ark-redis/lib/cdk-ark-redis-stack.ts
+++ infrastructure/cdk-ark-redis/lib/cdk-ark-redis-stack.ts
@@ -7,7 +7,7 @@ export class ArkRedisStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = Vpc.fromLookup(scope, "ArkVPC", {
+    const vpc = Vpc.fromLookup(this, "ArkVPC", {
       vpcId: "vpc-0d11f7ec183208e08",
     });
 
@@ -23,7 +23,7 @@ export class ArkRedisStack extends cdk.Stack {
       "Allow Redis access"
     );
 
-    const redisCluster = new CfnCacheCluster(this, "RedisCluster", {
+    new CfnCacheCluster(this, "RedisCluster", {
       engine: "redis",
       cacheNodeType: "cache.t3.micro",
       numCacheNodes: 1,
