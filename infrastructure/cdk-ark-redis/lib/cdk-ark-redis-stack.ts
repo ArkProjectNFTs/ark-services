dc2cf1794a0a4f6c34aded6fc854bd76981e6a7b
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2024-01-09 18:38:47 +0100

    chore: fix vpc subnets (#148)
    chore: fix subnets

--- infrastructure/cdk-ark-redis/lib/cdk-ark-redis-stack.ts
+++ infrastructure/cdk-ark-redis/lib/cdk-ark-redis-stack.ts
@@ -7,31 +7,38 @@ export class ArkRedisStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Utilisation de Vpc.fromLookup pour obtenir un VPC existant par son ID
     const vpc = Vpc.fromLookup(this, "ArkVPC", {
       vpcId: "vpc-0d11f7ec183208e08",
     });
 
+    // Création d'un Security Group pour Redis dans ce VPC
     const securityGroup = new SecurityGroup(this, "RedisSecurityGroup", {
       vpc,
       description: "Allow redis access",
       allowAllOutbound: true,
     });
 
+    // Autoriser les connexions entrantes sur le port 6379 pour Redis
     securityGroup.addIngressRule(
       Peer.anyIpv4(),
       Port.tcp(6379),
       "Allow Redis access"
     );
 
+    // Création du Subnet Group pour Redis
+    const redisSubnetGroup = new CfnSubnetGroup(this, "RedisSubnetGroup", {
+      description: "Subnet group for Redis",
+      subnetIds: vpc.privateSubnets.map((subnet) => subnet.subnetId),
+    });
+
+    // Création du cluster Redis
     new CfnCacheCluster(this, "RedisCluster", {
       engine: "redis",
       cacheNodeType: "cache.t3.micro",
       numCacheNodes: 1,
       vpcSecurityGroupIds: [securityGroup.securityGroupId],
-      cacheSubnetGroupName: new CfnSubnetGroup(this, "RedisSubnetGroup", {
-        description: "Subnet group for Redis",
-        subnetIds: vpc.publicSubnets.map((subnet) => subnet.subnetId),
-      }).cacheSubnetGroupName,
+      cacheSubnetGroupName: redisSubnetGroup.cacheSubnetGroupName,
     });
   }
 }
