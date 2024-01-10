45a14bfa8654d731a8a218102cef86ca22002e99
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2024-01-10 13:38:36 +0100

    fix(ark-lambdas): add redis access (#153)
    * fix: redis deployment
    
    * fix(ark-lambdas): add redis access

--- infrastructure/cdk-ark-redis/lib/cdk-ark-redis-stack.ts
+++ infrastructure/cdk-ark-redis/lib/cdk-ark-redis-stack.ts
@@ -1,15 +1,34 @@
 import * as cdk from "aws-cdk-lib";
-import { CfnCacheCluster } from "aws-cdk-lib/aws-elasticache";
+import { SecurityGroup, Vpc } from "aws-cdk-lib/aws-ec2";
+import { CfnCacheCluster, CfnSubnetGroup } from "aws-cdk-lib/aws-elasticache";
 import { Construct } from "constructs";
 
 export class ArkRedisStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CfnCacheCluster(this, "RedisCluster", {
+    const vpc = Vpc.fromLookup(this, "ArkVPC", {
+      vpcId: "vpc-0d11f7ec183208e08",
+    });
+
+    const subnetGroup = new CfnSubnetGroup(this, "ArkRedisSubnetGroup", {
+      description: "Subnet Group for Redis Cluster",
+      subnetIds: vpc.privateSubnets.map((subnet) => subnet.subnetId),
+    });
+
+    const redisSecurityGroup = new SecurityGroup(this, "RedisSecurityGroup", {
+      vpc,
+      description: "Security group for Redis Cluster",
+      allowAllOutbound: true,
+    });
+
+    new CfnCacheCluster(this, "ProdRedisCluster", {
+      clusterName: "ProdRedisCluster",
       engine: "redis",
-      cacheNodeType: "cache.t3.micro",
+      cacheNodeType: "cache.t2.micro",
       numCacheNodes: 1,
+      cacheSubnetGroupName: subnetGroup.ref,
+      vpcSecurityGroupIds: [redisSecurityGroup.securityGroupId],
     });
   }
 }
