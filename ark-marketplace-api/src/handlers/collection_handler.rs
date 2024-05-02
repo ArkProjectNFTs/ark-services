29510b0da4f12154b6d557c57edf35fc01f5ae65
bexan <y.bensacq@gmail.com>
2024-05-02 10:56:49 +0200

    ark-marketplace-api: start api

--- ark-marketplace-api/src/handlers/collection_handler.rs
+++ ark-marketplace-api/src/handlers/collection_handler.rs
@@ -1,67 +1,72 @@
 use crate::db::db_access::DatabaseAccess;
-use crate::db::query::get_collection_data;
+use crate::db::query::{
+    delete_migrations_query, delete_token_data, flush_all_data_query, get_token_by_collection_data,
+    get_token_data, get_token_history_data, get_token_offers_data, get_tokens_by_account_data,
+};
+use crate::utils::http_utils::convert_param_to_hex;
 use actix_web::{web, HttpResponse, Responder};
-use serde::Deserialize;
 
-use crate::models::collection::CollectionData;
-#[derive(Deserialize)]
-pub struct Pagination {
-    page: Option<i64>,
-    items_per_page: Option<i64>,
-}
-
-pub async fn get_collection<D: DatabaseAccess + Sync>(
-    pagination: web::Query<Pagination>,
+pub async fn get_token<D: DatabaseAccess + Sync>(
+    path: web::Path<(String, String)>,
     db_pool: web::Data<D>,
 ) -> impl Responder {
-    let page = pagination.page.unwrap_or(1);
-    let items_per_page = pagination.items_per_page.unwrap_or(100);
+    let (token_address, token_id) = path.into_inner();
 
-    let db_access = db_pool.get_ref();
-    match get_collection_data(db_access, page, items_per_page).await {
-        Err(sqlx::Error::RowNotFound) => HttpResponse::NotFound().body("data not found"),
-        Ok(collection_data) => HttpResponse::Ok().json(collection_data),
+    match convert_param_to_hex(&token_id) {
+        Ok(token_id_hex) => {
+            let db_access = db_pool.get_ref();
+            match get_token_data(db_access, &token_address, &token_id_hex).await {
+                Err(sqlx::Error::RowNotFound) => HttpResponse::NotFound().body("data not found"),
+                Ok(token_data) => HttpResponse::Ok().json(token_data),
+                Err(_) => HttpResponse::InternalServerError().finish(),
+            }
+        }
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
+
 #[cfg(test)]
 mod tests {
     use crate::db::db_access::MockDb;
-    use crate::handlers::collection_handler::get_collection;
-    use crate::models::collection::CollectionData;
+    use crate::handlers::token_handler::{
+        get_token, get_token_history, get_token_offers, get_tokens_by_account,
+        get_tokens_by_collection,
+    };
+    use crate::models::token::{TokenData, TokenWithHistory, TokenWithOffers};
     use actix_web::{http, test, web, App};
 
     #[actix_rt::test]
-    async fn test_get_collection_handler() {
+    async fn test_get_token_handler() {
         let app = test::init_service(
             App::new()
                 .app_data(web::Data::new(MockDb))
-                .route("/collections", web::get().to(get_collection::<MockDb>)),
+                .route("/token/{address}/{id}", web::get().to(get_token::<MockDb>)),
         )
         .await;
 
-        let req = test::TestRequest::get().uri("/collections").to_request();
+        let req = test::TestRequest::get()
+            .uri("/token/0xABCDEF123456/0xABCDEF123456")
+            .to_request();
         let resp = test::call_service(&app, req).await;
         let status = resp.status();
         assert_eq!(status, http::StatusCode::OK);
         let response_body = test::read_body(resp).await;
-        let collection_data: Vec<CollectionData> = serde_json::from_slice(&response_body).unwrap();
-        assert_eq!(
-            collection_data[0].image,
-            Some("https://example.com/image.png".to_string())
-        );
-        assert_eq!(
-            collection_data[0].collection_name,
-            Some("Example Collection".to_string())
-        );
-        assert_eq!(collection_data[0].floor, Some("1".to_string()));
-        assert_eq!(collection_data[0].floor_7d_percentage, Some(4));
-        assert_eq!(collection_data[0].volume_7d_eth, Some(789));
-        assert_eq!(collection_data[0].top_offer, Some("Top Offer".to_string()));
-        assert_eq!(collection_data[0].sales_7d, Some(10));
-        assert_eq!(collection_data[0].marketcap, Some(1112));
-        assert_eq!(collection_data[0].listed_items, Some(13));
-        assert_eq!(collection_data[0].listed_percentage, Some(14));
+        let token_data: TokenData = serde_json::from_slice(&response_body).unwrap();
+        assert_eq!(token_data.token_chain_id, "chainXYZ");
+        assert_eq!(token_data.token_address, "0xABCDEF123456");
+        assert_eq!(token_data.token_id, "789");
+        assert_eq!(token_data.listed_timestamp, 1234567890);
+        assert_eq!(token_data.updated_timestamp, 1234567891);
+        assert_eq!(token_data.current_owner, Some("owner123".to_string()));
+        assert_eq!(token_data.last_price, Some("100".to_string()));
+        assert_eq!(token_data.quantity, Some("10".to_string()));
+        assert_eq!(token_data.start_amount, Some("50".to_string()));
+        assert_eq!(token_data.end_amount, Some("150".to_string()));
+        assert_eq!(token_data.start_date, Some(1234567890));
+        assert_eq!(token_data.end_date, Some(1234567891));
+        assert_eq!(token_data.is_listed, Some(true));
+        assert_eq!(token_data.has_offer, Some(false));
+        assert_eq!(token_data.broker_id, Some("brokerXYZ".to_string()));
     }
 }
