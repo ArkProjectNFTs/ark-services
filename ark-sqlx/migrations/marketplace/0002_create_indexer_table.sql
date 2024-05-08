ee3fc1133be8d21324b8499950aa519ea439b2f6
remiroyc <remiroycourt@gmail.com>
2024-05-08 02:44:22 +0200

    fix: sql migrations

--- ark-sqlx/migrations/marketplace/0002_create_indexer_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE indexer (
-     task_id SERIAL PRIMARY KEY,
-     status TEXT NOT NULL,
-     last_update BIGINT NOT NULL,
-     version TEXT NOT NULL,
-     indexation_progress REAL,
-     current_block_number BIGINT
-);
--- ark-sqlx/migrations/marketplace/0_marketplace.sql
+++ ark-sqlx/migrations/marketplace/0_marketplace.sql
@@ -1,6 +1,6 @@
 CREATE TABLE contract (
   contract_address CHAR(66) PRIMARY KEY,
-  chain_id TEXT,
+  chain_id TEXT NOT NULL,
   updated_timestamp BIGINT NOT NULL DEFAULT (EXTRACT(EPOCH FROM NOW())::BIGINT),
   floor_price TEXT,
   top_bid TEXT,
@@ -14,9 +14,9 @@ CREATE TABLE contract (
 );
 
 CREATE TABLE token (
-   token_chain_id TEXT,
    contract_address CHAR(66) NOT NULL,
-   token_id TEXT NOT NULL,
+   token_id NUMERIC NOT NULL,
+   token_id_hex TEXT NOT NULL,
    current_owner TEXT,
    last_price TEXT NULL,
    currency_address TEXT NOT NULL DEFAULT '',
@@ -38,7 +38,6 @@ CREATE TABLE token (
    listing_end_date BIGINT NULL,
    metadata JSON NULL,
    metadata_ok BOOLEAN NOT NULL DEFAULT FALSE,
-   token_id_hex TEXT NOT NULL,
    top_bid_amount TEXT NULL,
    top_bid_broker_id TEXT NULL,
    top_bid_order_hash TEXT NOT NULL DEFAULT '',
@@ -53,10 +52,10 @@ CREATE TABLE token_event (
   event_id SERIAL PRIMARY KEY,
   contract_address CHAR(66) NOT NULL,
   order_hash TEXT NOT NULL DEFAULT '',
-  token_id TEXT NOT NULL,
+  token_id NUMERIC NOT NULL,
+  token_id_hex TEXT NOT NULL,
   event_type TEXT NOT NULL CHECK (event_type IN ('Listing', 'CollectionOffer', 'Offer', 'Auction', 'Fulfill', 'Cancelled', 'Executed', 'Buy', 'Sell', 'Mint', 'Burn', 'Transfer')),
   timestamp BIGINT NOT NULL,
-  token_id_hex TEXT NOT NULL,
   transaction_hash TEXT NULL,
   to_address TEXT, -- NULL if not transfert
   from_address TEXT, -- NULL if new listing
@@ -68,7 +67,7 @@ CREATE TABLE token_event (
 CREATE TABLE token_offer (
   offer_id SERIAL PRIMARY KEY,
   contract_address CHAR(66) NOT NULL,
-  token_id TEXT NOT NULL,
+  token_id NUMERIC NOT NULL,
   order_hash TEXT NOT NULL DEFAULT '',
   offer_maker TEXT NOT NULL,
   offer_amount TEXT NOT NULL,
@@ -82,10 +81,24 @@ CREATE TABLE token_offer (
   FOREIGN KEY (contract_address, token_id) REFERENCES token(contract_address, token_id)
 );
 
-CREATE TABLE block (
-    block_number SERIAL PRIMARY KEY,
-    indexer_identifier TEXT NOT NULL,
-    indexer_version TEXT NOT NULL,
-    status TEXT NOT NULL,
-    timestamp BIGINT NOT NULL
+CREATE TABLE indexer (
+  indexer_identifier TEXT PRIMARY KEY,
+  indexer_status TEXT,
+  last_updated_timestamp BIGINT,
+  created_timestamp BIGINT NOT NULL DEFAULT (EXTRACT(EPOCH FROM NOW())::BIGINT),
+  indexer_version TEXT, 
+  indexation_progress_percentage NUMERIC(5, 2) DEFAULT 0.00,
+  current_block_number BIGINT,
+  is_force_mode_enabled BOOLEAN NOT NULL DEFAULT FALSE, 
+  start_block_number BIGINT, 
+  end_block_number BIGINT
 );
+
+CREATE TABLE block (
+  block_number BIGINT PRIMARY KEY,
+  block_status TEXT,
+  block_timestamp BIGINT NOT NULL, 
+  indexer_identifier TEXT,
+  FOREIGN KEY (indexer_identifier) REFERENCES indexer(indexer_identifier)
+);
+
