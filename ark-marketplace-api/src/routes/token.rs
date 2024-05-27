f8b71b1cd4ca530764f6750b4fe77ae7db4aaaf0
bexan <y.bensacq@gmail.com>
2024-05-28 01:32:10 +0200

    token: add buy now filter & sort & price in struct

--- ark-marketplace-api/src/routes/token.rs
+++ ark-marketplace-api/src/routes/token.rs
@@ -4,5 +4,8 @@ use sqlx::PgPool;
 use crate::handlers::token_handler;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.route("/collection/{address}/tokens", web::get().to(token_handler::get_tokens::<PgPool>));
+    cfg.route(
+        "/collection/{address}/tokens",
+        web::get().to(token_handler::get_tokens::<PgPool>),
+    );
 }
