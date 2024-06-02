0d44a069a3ccab36e946100e746e05539488883a
bexan <y.bensacq@gmail.com>
2024-06-02 10:26:43 +0200

    marketplace-api: collection put 0 if value is null

--- ark-marketplace-api/src/models/collection.rs
+++ ark-marketplace-api/src/models/collection.rs
@@ -5,10 +5,10 @@ use sqlx::FromRow;
 pub struct CollectionData {
     pub image: Option<String>,
     pub collection_name: Option<String>,
-    pub floor: Option<String>,
+    pub floor: Option<i32>,
     pub floor_7d_percentage: Option<i32>,
     pub volume_7d_eth: Option<i32>,
-    pub top_offer: Option<String>,
+    pub top_offer: Option<i32>,
     pub sales_7d: Option<i64>,
     pub marketcap: Option<i32>,
     pub listed_items: Option<i64>,
