22b34c977a6f9db8ea50e0cafa8883102906d189
bexan <y.bensacq@gmail.com>
2024-06-10 12:14:48 +0200

    marketplace-api: use normalized address for other endpoints

--- ark-marketplace-api/src/utils/http_utils.rs
+++ ark-marketplace-api/src/utils/http_utils.rs
@@ -14,3 +14,25 @@ pub fn convert_param_to_hex(param: &str) -> Result<String, &'static str> {
         }
     }
 }
+
+/// Normalize an Ethereum address to a consistent length of 66 characters (including the '0x' prefix)
+/// by padding with leading zeros. This is useful for ensuring that Ethereum addresses are always
+/// represented in the same way, regardless of how they were input or received.
+///
+/// # Arguments
+///
+/// * `address` - An address as a string. It should start with '0x'.
+///
+/// # Returns
+///
+/// * A string representing the normalized Ethereum address.
+pub fn normalize_address(address: &str) -> String {
+    let prefix = &address[0..2];
+    let hex = &address[2..];
+
+    let hex = format!("{:0>64}", hex);
+
+    let normalized_address = format!("{}{}", prefix, hex);
+
+    normalized_address
+}
