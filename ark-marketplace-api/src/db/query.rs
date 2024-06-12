0601b90463e9cc72f887c5a5c22b6abf78e5aebc
bexan <y.bensacq@gmail.com>
2024-06-12 15:56:45 +0300

    feat(marketplace-api): rework sql query for portfolio collection (#364)
    marketplace-api: rework sql query for portfolio collection

--- ark-marketplace-api/src/db/query.rs
+++ ark-marketplace-api/src/db/query.rs
@@ -1,5 +1,5 @@
 use crate::db::db_access::DatabaseAccess;
-use crate::models::collection::CollectionData;
+use crate::models::collection::{CollectionData, CollectionPortfolioData};
 use crate::models::token::{TokenData, TokenPortfolioData};
 
 pub async fn get_collections_data<D: DatabaseAccess + Sync>(
@@ -18,10 +18,9 @@ pub async fn get_portfolio_collections_data<D: DatabaseAccess + Sync>(
     user_address: &str,
     page: i64,
     items_per_page: i64,
-    time_range: &str,
-) -> Result<Vec<CollectionData>, sqlx::Error> {
+) -> Result<Vec<CollectionPortfolioData>, sqlx::Error> {
     db_access
-        .get_collections_data(page, items_per_page, time_range, Some(user_address))
+        .get_portfolio_collections_data(page, items_per_page, user_address)
         .await
 }
 
