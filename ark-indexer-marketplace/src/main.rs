9b74b571cc6e3ec8ec870264856752fedda34b5f
Rémi Roycourt <remiroycourt@gmail.com>
2024-06-17 12:48:41 +0200

    fix(ark-indexer-marketplace): indexer_identifier

--- ark-indexer-marketplace/src/main.rs
+++ ark-indexer-marketplace/src/main.rs
@@ -82,8 +82,8 @@ async fn main() -> Result<()> {
     let starknet_client = Arc::new(StarknetClientHttp::new(rpc_url.as_str())?);
 
     let sana_observer = Arc::new(SanaObserver::new(
-        Some(indexer_version.clone()),
-        Some(indexer_identifier.clone()),
+        indexer_version.clone(),
+        indexer_identifier.clone(),
     ));
 
     let provider = Arc::new(AnyProvider::JsonRpcHttp(JsonRpcClient::new(
