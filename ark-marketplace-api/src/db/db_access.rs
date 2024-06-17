d61e682ff9eca4ac97d6103f0fcd0d80e9c4d1bb
bexan <y.bensacq@gmail.com>
2024-06-18 00:28:05 +0200

    fix(marketplace-api): optimize query

--- ark-marketplace-api/src/db/db_access.rs
+++ ark-marketplace-api/src/db/db_access.rs
@@ -439,10 +439,7 @@ impl DatabaseAccess for PgPool {
                    (EXTRACT(EPOCH FROM CURRENT_TIMESTAMP) BETWEEN listing_start_date AND listing_end_date)
                )
                ORDER BY
-               CASE
-                  WHEN EXTRACT(EPOCH FROM CURRENT_TIMESTAMP) BETWEEN token.listing_start_date AND token.listing_end_date THEN 1
-                  ELSE 2
-               END,
+                   token.listing_end_date desc nulls last,
                CASE
                    WHEN $6 = 'price' THEN
                        CASE WHEN $7 = 'asc' THEN token.listing_start_amount
