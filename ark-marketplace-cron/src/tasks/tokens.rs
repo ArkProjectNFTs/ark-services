221eb05c3e715941d7611f8c6012d17f2467ce97
bexan <y.bensacq@gmail.com>
2024-07-04 20:33:55 +0200

    feat(ark-marketplace-cron): reset listing fields

--- ark-marketplace-cron/src/tasks/tokens.rs
+++ ark-marketplace-cron/src/tasks/tokens.rs
@@ -9,24 +9,18 @@ const CHAIN_ID: &str = "0x534e5f4d41494e";
 const ITEMS_PER_PAGE: i64 = 50;
 
 pub async fn update_listed_tokens(pool: &PgPool) {
-    let update_is_listed_query = r#"
-        UPDATE token
-        SET is_listed = CASE
-            WHEN NOW() BETWEEN to_timestamp(listing_start_date) AND to_timestamp(listing_end_date) THEN true
-            ELSE false
-        END
-        WHERE listing_start_date IS NOT NULL AND listing_end_date IS NOT NULL;
-    "#;
-
-    match sqlx::query(update_is_listed_query).execute(pool).await {
-        Ok(_) => info!("Update of is_listed field successful."),
-        Err(e) => tracing::error!("Failed to update is_listed field: {}", e),
-    }
 
     let clean_dates_query = r#"
         UPDATE token
         SET listing_start_date = NULL,
-            listing_end_date = NULL
+            listing_end_date = NULL,
+            listing_timestamp = NULL,
+            listing_start_amount = NULL,
+            listing_end_amount = NULL,
+            listing_currency_address = NULL,
+            listing_currency_chain_id = NULL,
+            listing_type = NULL,
+            listing_orderhash = NULL
         WHERE (NOW() > to_timestamp(listing_end_date) OR NOW() < to_timestamp(listing_start_date))
           AND listing_start_date IS NOT NULL AND listing_end_date IS NOT NULL;
     "#;
