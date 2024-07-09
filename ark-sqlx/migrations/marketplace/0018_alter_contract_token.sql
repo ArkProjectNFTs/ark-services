da92a68027341bd662801065f5afcbc73766fe6f
bexan <y.bensacq@gmail.com>
2024-07-09 17:11:54 +0200

    fix: remove index

--- ark-sqlx/migrations/marketplace/0018_alter_contract_token.sql
+++ ark-sqlx/migrations/marketplace/0018_alter_contract_token.sql
@@ -3,5 +3,9 @@ ALTER COLUMN volume_7d_eth TYPE numeric,
 ALTER COLUMN marketcap TYPE numeric;
 
 
+drop index idx_token_is_listed;
+drop index idx_token_order;
+
+
 ALTER TABLE token
     DROP COLUMN is_listed;
