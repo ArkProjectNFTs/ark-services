32f07e974bdd434629a97263da019702297bc2dc
bexan <y.bensacq@gmail.com>
2024-07-10 09:21:00 +0200

    remove useless migration

--- ark-sqlx/migrations/marketplace/0018_alter_contract_token.sql
+++ ark-sqlx/migrations/marketplace/0018_alter_contract_token.sql
@@ -1,11 +1,6 @@
 ALTER TABLE contract
-    ALTER COLUMN volume_7d_eth TYPE numeric,
-    ALTER COLUMN marketcap TYPE numeric;
-
-
-drop index idx_token_is_listed;
-drop index idx_token_order;
-drop materialized view token_materialized_view;
+ALTER COLUMN volume_7d_eth TYPE numeric,
+ALTER COLUMN marketcap TYPE numeric;
 
 
 drop index idx_token_is_listed;
