c90621d94cffd816364c0255364467a3ee786d17
bexan <y.bensacq@gmail.com>
2024-08-08 14:54:31 +0200

    feat(marketplace-api): start documentation

--- ark-marketplace-api/src/handlers/default_handler.rs
+++ ark-marketplace-api/src/handlers/default_handler.rs
@@ -1,5 +1,19 @@
 use actix_web::{HttpResponse, Responder};
+use actix_web::web::{Json, Path};
+use actix_web::Error;
+use apistos::actix::CreatedJson;
+use apistos::{api_operation, ApiComponent};
+use schemars::JsonSchema;
 
-pub async fn health_check() -> impl Responder {
-    HttpResponse::Ok().json(serde_json::json!({"status": "ok"}))
+
+#[derive(serde::Serialize, JsonSchema)]
+struct HealthCheckResponse {
+    status: String,
+}
+
+#[api_operation(summary = "Health Check")]
+async fn health_check() -> Result<CreatedJson<HealthCheckResponse>, Error> {
+    HttpResponse::Ok().json(HealthCheckResponse {
+        status: "ok".to_string(),
+    })
 }
