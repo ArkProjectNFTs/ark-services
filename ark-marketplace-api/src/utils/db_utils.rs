f601d754dd1916838fda406507c59ace20e270ee
Patrice Tisserand <p.tisserand@gmail.com>
2024-08-19 10:21:01 +0200

    DRY

--- ark-marketplace-api/src/utils/db_utils.rs
+++ ark-marketplace-api/src/utils/db_utils.rs
@@ -1,4 +1,5 @@
 use crate::models::token::TokenEventType;
+use ark_sqlx::providers::marketplace::types::TokenEventType as TokenEventTypeDB;
 
 pub fn event_type_list(values: &[TokenEventType]) -> String {
     let mut types = values
@@ -6,29 +7,36 @@ pub fn event_type_list(values: &[TokenEventType]) -> String {
         .map(|v| format!("'{}'", v.to_db_string()))
         .collect::<Vec<_>>();
 
-    if types.contains(&format!("'{}'", TOKEN_EVENT_SALE_STR)) {
-        types.push(format!("'{}'", TOKEN_EVENT_EXECUTED_STR));
+    if types.contains(&format!("'{}'", TokenEventType::Sale.to_db_string())) {
+        types.push(format!("'{}'", TokenEventType::Executed.to_db_string()));
     }
 
     types.join(", ")
 }
 
-/// DB conversion for TokenEventType
-const TOKEN_EVENT_LISTING_STR: &str = "Listing";
-const TOKEN_EVENT_COLLECTION_OFFER_STR: &str = "CollectionOffer";
-const TOKEN_EVENT_OFFER_STR: &str = "Offer";
-const TOKEN_EVENT_AUCTION_STR: &str = "Auction";
-const TOKEN_EVENT_FULFILL_STR: &str = "Fulfill";
-const TOKEN_EVENT_CANCELLED_STR: &str = "Cancelled";
-const TOKEN_EVENT_EXECUTED_STR: &str = "Executed";
-const TOKEN_EVENT_SALE_STR: &str = "Sale";
-const TOKEN_EVENT_MINT_STR: &str = "Mint";
-const TOKEN_EVENT_BURN_STR: &str = "Burn";
-const TOKEN_EVENT_TRANSFER_STR: &str = "Transfer";
-// Cancel event
-const TOKEN_EVENT_LISTING_CANCELLED_STR: &str = "ListingCancelled";
-const TOKEN_EVENT_AUCTION_CANCELLED_STR: &str = "AuctionCancelled";
-const TOKEN_EVENT_OFFER_CANCELLED_STR: &str = "OfferCancelled";
+
+/// Convert TokenEventType to matching keys in DB
+impl TokenEventType {
+    pub fn to_db_string(&self) -> String {
+        match self {
+            Self::Listing => TokenEventTypeDB::Listing.to_db_string(),
+            Self::CollectionOffer => TokenEventTypeDB::CollectionOffer.to_db_string(),
+            Self::Offer => TokenEventTypeDB::Offer.to_string(),
+            Self::Auction => TokenEventTypeDB::Auction.to_string(),
+            Self::Fulfill => TokenEventTypeDB::Fulfill.to_string(),
+            Self::Cancelled => TokenEventTypeDB::Cancelled.to_string(),
+            Self::Executed => TokenEventTypeDB::Executed.to_string(),
+            Self::Sale => TokenEventTypeDB::Sale.to_string(),
+            Self::Mint => TokenEventTypeDB::Mint.to_string(),
+            Self::Burn => TokenEventTypeDB::Burn.to_string(),
+            Self::Transfer => TokenEventType::Transfer.to_string(),
+            // Cancel event
+            Self::ListingCancelled => TokenEventTypeDB::ListingCancelled.to_string(),
+            Self::AuctionCancelled => TokenEventTypeDB::AuctionCancelled.to_string(),
+            Self::OfferCancelled => TokenEventTypeDB::OfferCancelled.to_string(),
+        }
+    }
+}
 
 impl<DB> sqlx::Type<DB> for TokenEventType
 where
@@ -48,47 +56,27 @@ where
     fn decode(
         value: <DB as sqlx::database::HasValueRef<'r>>::ValueRef,
     ) -> Result<Self, sqlx::error::BoxDynError> {
-        let s = <&str as sqlx::Decode<DB>>::decode(value)?;
-        match s {
-            TOKEN_EVENT_LISTING_STR => Ok(TokenEventType::Listing),
-            TOKEN_EVENT_COLLECTION_OFFER_STR => Ok(TokenEventType::CollectionOffer),
-            TOKEN_EVENT_OFFER_STR => Ok(TokenEventType::Offer),
-            TOKEN_EVENT_AUCTION_STR => Ok(TokenEventType::Auction),
-            TOKEN_EVENT_FULFILL_STR => Ok(TokenEventType::Fulfill),
-            TOKEN_EVENT_CANCELLED_STR => Ok(TokenEventType::Cancelled),
-            TOKEN_EVENT_EXECUTED_STR => Ok(TokenEventType::Executed),
-            TOKEN_EVENT_SALE_STR => Ok(TokenEventType::Sale),
-            TOKEN_EVENT_MINT_STR => Ok(TokenEventType::Mint),
-            TOKEN_EVENT_BURN_STR => Ok(TokenEventType::Burn),
-            TOKEN_EVENT_TRANSFER_STR => Ok(TokenEventType::Transfer),
-            // Cancel event
-            TOKEN_EVENT_LISTING_CANCELLED_STR => Ok(TokenEventType::ListingCancelled),
-            TOKEN_EVENT_AUCTION_CANCELLED_STR => Ok(TokenEventType::AuctionCancelled),
-            TOKEN_EVENT_OFFER_CANCELLED_STR => Ok(TokenEventType::OfferCancelled),
-            _ => Err("Invalid event type".into()),
-        }
-    }
-}
-
-/// Convert TokenEventType to matching keys in DB
-impl TokenEventType {
-    pub fn to_db_string(&self) -> String {
-        match self {
-            Self::Listing => TOKEN_EVENT_LISTING_STR.to_string(),
-            Self::CollectionOffer => TOKEN_EVENT_COLLECTION_OFFER_STR.to_string(),
-            Self::Offer => TOKEN_EVENT_OFFER_STR.to_string(),
-            Self::Auction => TOKEN_EVENT_AUCTION_STR.to_string(),
-            Self::Fulfill => TOKEN_EVENT_FULFILL_STR.to_string(),
-            Self::Cancelled => TOKEN_EVENT_CANCELLED_STR.to_string(),
-            Self::Executed => TOKEN_EVENT_EXECUTED_STR.to_string(),
-            Self::Sale => TOKEN_EVENT_SALE_STR.to_string(),
-            Self::Mint => TOKEN_EVENT_MINT_STR.to_string(),
-            Self::Burn => TOKEN_EVENT_BURN_STR.to_string(),
-            Self::Transfer => TOKEN_EVENT_TRANSFER_STR.to_string(),
-            // Cancel event
-            Self::ListingCancelled => TOKEN_EVENT_LISTING_CANCELLED_STR.to_string(),
-            Self::AuctionCancelled => TOKEN_EVENT_AUCTION_CANCELLED_STR.to_string(),
-            Self::OfferCancelled => TOKEN_EVENT_OFFER_CANCELLED_STR.to_string(),
+        if let Ok(e) = TokenEventTypeDB::decode(value) {
+            match e {
+                TokenEventTypeDB::Listing => Ok(TokenEventType::Listing),
+                TokenEventTypeDB::Auction => Ok(TokenEventType::Auction),
+                TokenEventTypeDB::Offer => Ok(TokenEventType::Offer),
+                TokenEventTypeDB::CollectionOffer => Ok(TokenEventType::CollectionOffer),
+                TokenEventTypeDB::Fulfill => Ok(TokenEventType::Offer),
+                TokenEventTypeDB::Executed => Ok(TokenEventType::Executed),
+                TokenEventTypeDB::Cancelled => Ok(TokenEventType::Cancelled),
+                TokenEventTypeDB::Sale => Ok(TokenEventType::Sale),
+                TokenEventTypeDB::Mint => Ok(TokenEventType::Mint),
+                TokenEventTypeDB::Burn => Ok(TokenEventType::Burn),
+                TokenEventTypeDB::Transfer => Ok(TokenEventType::Transfer),
+                TokenEventTypeDB::ListingCancelled => Ok(TokenEventType::ListingCancelled),
+                TokenEventTypeDB::AuctionCancelled => Ok(TokenEventType::AuctionCancelled),
+                TokenEventTypeDB::OfferCancelled => Ok(TokenEventType::OfferCancelled),
+                TokenEventTypeDB::Rollback => Err("Unsupported rollback event".into())
+                // _ => Ok(TokenEventType::Burn),
+            }
+        } else {
+            Err("Invalid event type".into())
         }
     }
 }
