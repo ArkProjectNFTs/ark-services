8755c30d8ddde6fb1006afd2f2ee169e34935184
bexan <y.bensacq@gmail.com>
2024-08-22 12:18:43 +0200

    feat(ark-metadata-marketplace): collection filters with metadata (#527)
    * feat(ark-metadata-marketplace): add elasticsearch manager
    
    * feat(marketplace-api): add endpoint to get all traits and values in a collection
    
    * update arkproject tag

---
+++ ark-metadata-marketplace/src/elasticsearch_manager.rs
@@ -0,0 +1,67 @@
+use arkproject::metadata::{
+    elasticsearch_manager::ElasticsearchManager,
+    types::{RequestError, TokenMetadata},
+};
+use async_trait::async_trait;
+use reqwest::Client as ReqwestClient;
+use serde_json::json;
+
+pub struct EsManager {
+    client: ReqwestClient,
+    elasticsearch_url: String,
+    username: String,
+    password: String,
+}
+
+#[async_trait]
+impl ElasticsearchManager for EsManager {
+    async fn upsert_token_metadata(
+        &self,
+        contract_address: &str,
+        token_id: &str,
+        chain_id: &str,
+        metadata: TokenMetadata,
+    ) -> Result<(), RequestError> {
+        let doc_id = format!("{}_{}_{}", contract_address, chain_id, token_id);
+        let url = format!("{}/nft-metadata/_update/{}", self.elasticsearch_url, doc_id);
+
+        let response = self
+            .client
+            .post(&url)
+            .basic_auth(&self.username, Some(&self.password))
+            .json(&json!({
+                "doc": {
+                    "contract_address": contract_address,
+                    "token_id": token_id,
+                    "chain_id": chain_id,
+                    "metadata": metadata.normalized,
+                    "raw_metadata": metadata.raw,
+                    "metadata_updated_at": metadata.metadata_updated_at
+                },
+                "doc_as_upsert": true
+            }))
+            .send()
+            .await?;
+
+        if response.status().is_success() {
+            Ok(())
+        } else {
+            let error_message = response
+                .text()
+                .await
+                .unwrap_or_else(|_| "Unknown error".to_string());
+            Err(RequestError::Reqwest(error_message))
+        }
+    }
+}
+
+impl EsManager {
+    pub fn new(elasticsearch_url: String, username: String, password: String) -> Self {
+        Self {
+            client: ReqwestClient::new(),
+            elasticsearch_url,
+            username,
+            password,
+        }
+    }
+}
