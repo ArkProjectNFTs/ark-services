5d51d77fa0043d18ff288842cf0223c87c6b0134
bexan <y.bensacq@gmail.com>
2024-08-29 16:57:33 +0200

    feat(marketplace-api): update route order (#547)
    * feat(marketplace-api): rename metadata endpoints
    
    * feat(marketplace-api): move tests

--- ark-marketplace-api/src/routes/collection.rs
+++ ark-marketplace-api/src/routes/collection.rs
@@ -30,6 +30,16 @@ pub fn config(cfg: &mut web::ServiceConfig) {
     );
 
     cfg.route(
+        "/collections/{address}/traits",
+        web::get().to(collection_handler::get_traits::<PgPool>),
+    );
+
+    cfg.route(
+        "/collections/{address}/filters",
+        web::get().to(collection_handler::get_filtered_tokens::<PgPool>),
+    );
+
+    cfg.route(
         "/portfolio/{user_address}/collections",
         web::get().to(collection_handler::get_portfolio_collections::<PgPool>),
     );
