b68a028f58634011624a4bf1b6ea4d761b5b3d95
bexan <y.bensacq@gmail.com>
2024-08-29 16:06:42 +0200

    feat(marketplace-api): rename metadata endpoints (#545)

--- ark-marketplace-api/src/routes/collection.rs
+++ ark-marketplace-api/src/routes/collection.rs
@@ -10,6 +10,16 @@ pub fn config(cfg: &mut web::ServiceConfig) {
     );
 
     cfg.route(
+        "/collections/{address}/traits",
+        web::get().to(collection_handler::get_traits::<PgPool>),
+    );
+
+    cfg.route(
+        "/collections/{address}/filters",
+        web::get().to(collection_handler::get_filtered_tokens::<PgPool>),
+    );
+
+    cfg.route(
         "/collections/{address}/activity",
         web::get().to(collection_handler::get_collection_activity::<PgPool>),
     );
