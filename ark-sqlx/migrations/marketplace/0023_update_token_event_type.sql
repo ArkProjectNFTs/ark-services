f628d73994ffc90f39eed101e593c2c5d7ff61c5
bexan <y.bensacq@gmail.com>
2024-09-03 10:16:09 +0200

    remove sql file

--- ark-sqlx/migrations/marketplace/0023_update_token_event_type.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE token_event
-DROP CONSTRAINT token_event_event_type_check,
-    ADD CONSTRAINT token_event_event_type_check CHECK (event_type IN ('Listing', 'CollectionOffer', 'Offer', 'Auction', 'Fulfill', 'Cancelled', 'Executed', 'Sale', 'Mint', 'Burn', 'Transfer', 'OfferExpired', 'ListingExpired'));
