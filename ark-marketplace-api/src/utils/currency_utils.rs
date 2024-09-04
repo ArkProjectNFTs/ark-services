47ebcd8f030bfbe31393d657cb39a7738c6433e9
bexan <y.bensacq@gmail.com>
2024-09-04 15:43:14 +0200

    fix(marketplace-api): offer percentage (#564)

--- ark-marketplace-api/src/utils/currency_utils.rs
+++ ark-marketplace-api/src/utils/currency_utils.rs
@@ -8,14 +8,19 @@ pub fn compute_floor_difference(
 ) -> Option<BigDecimal> {
     if currency_amount.is_none() || floor_price.is_none() {
         None
-    } else {
+    }
+    else {
         let floor_price = floor_price.unwrap();
         let price = currency_amount.unwrap(); // TODO: handle currency conversion
         let diff = price - floor_price.clone();
+        if diff.is_zero() {
+            return Some(BigDecimal::from(0));
+        }
         if floor_price.is_zero() {
             None
         } else {
-            Some(diff / floor_price)
+            let percentage_diff = (diff / floor_price) * BigDecimal::from(100);
+            Some(percentage_diff)
         }
     }
 }
