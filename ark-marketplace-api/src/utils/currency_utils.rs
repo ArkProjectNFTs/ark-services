ec931946db23af56fa47a6b78e8e43ebecb5bbf2
Mehdi AISSANI <mehdi@screenshot.co>
2024-09-05 14:13:20 +0000

    fix(lint): fix clippy and fmt errors

--- ark-marketplace-api/src/utils/currency_utils.rs
+++ ark-marketplace-api/src/utils/currency_utils.rs
@@ -9,8 +9,7 @@ pub fn compute_floor_difference(
 ) -> Option<BigDecimal> {
     if currency_amount.is_none() || floor_price.is_none() {
         None
-    }
-    else {
+    } else {
         let floor_price = floor_price.unwrap();
         let price = currency_amount.unwrap(); // TODO: handle currency conversion
         let diff = price - floor_price.clone();
@@ -22,7 +21,9 @@ pub fn compute_floor_difference(
         } else {
             let percentage_diff = (diff / floor_price) * BigDecimal::from(100);
             let percentage_diff_str = percentage_diff.to_string();
-            let trimmed_str = percentage_diff_str.trim_end_matches('0').trim_end_matches('.');
+            let trimmed_str = percentage_diff_str
+                .trim_end_matches('0')
+                .trim_end_matches('.');
             let rounded_percentage_diff = BigDecimal::from_str(trimmed_str).unwrap();
 
             Some(rounded_percentage_diff)
