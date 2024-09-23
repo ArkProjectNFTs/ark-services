81e1b99ca948f1b30e5d454741ea1d484144a3b0
Mehdi AISSANI <mehdi@screenshot.co>
2024-09-23 17:28:07 +0000

    fix(migration): duplicate 026

--- ark-sqlx/migrations/marketplace/0026_alter_transaction_tables.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
-ALTER TABLE transaction_info add COLUMN sub_event_id VARCHAR(78);
-UPDATE transaction_info set sub_event_id = concat(event_id, '_0');
-ALTER TABLE transaction_info ALTER COLUMN sub_event_id SET NOT NULL;
-ALTER TABLE transaction_info DROP CONSTRAINT transaction_info_tx_hash_event_id_key;
-ALTER TABLE transaction_info ADD CONSTRAINT transaction_info_tx_hash_event_id_sub_event_id_key UNIQUE(tx_hash, event_id, sub_event_id);
-COMMIT;
