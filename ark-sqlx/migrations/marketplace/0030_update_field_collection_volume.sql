c79abb0834f4432d3de437beb4c00a8ba2d5f6b2
Yannick Bensacq <y.bensacq@gmail.com>
2024-10-09 08:29:39 +0200

    update script total_volume

--- ark-sqlx/migrations/marketplace/0030_update_field_collection_volume.sql
+++ ark-sqlx/migrations/marketplace/0030_update_field_collection_volume.sql
@@ -1,2 +1,3 @@
-ALTER TABLE contract
-ALTER COLUMN total_volume TYPE numeric;
+ALTER TABLE contract ADD COLUMN total_volume_2 numeric;
+ALTER TABLE contract DROP COLUMN total_volume;
+ALTER TABLE contract RENAME COLUMN total_volume_2 TO total_volume;
