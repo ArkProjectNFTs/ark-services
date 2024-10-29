9c78c17dbec29733f20587faeb2da14494d8ffac
Mehdi Aïssani <mehdi@screenshot.co>
2024-10-29 17:13:04 +0100

    feat(ark-sqlx): ingest save token events in token events (#654)
    * chore(token_event): migrate token_transaction to token_event
    
    * chore(ark-indexer-transaction): store every contract token and token_event
    
    * fix(sqlx): prepare queries
    
    ---------
    
    Co-authored-by: remiroyc <remiroycourt@gmail.com>

--- ark-indexer-transactions/src/services/storage/mod.rs
+++ ark-indexer-transactions/src/services/storage/mod.rs
@@ -3,7 +3,7 @@ pub mod database;
 pub mod models;
 pub mod types;
 
-use crate::interfaces::contract::{NFTInfo, TransactionInfo};
+use crate::interfaces::contract::{ContractInfo, NFTInfo, TransactionInfo};
 
 use async_trait::async_trait;
 #[cfg(test)]
@@ -17,6 +17,18 @@ pub trait Storage {
         nft_info: NFTInfo,
     ) -> Result<(), Box<dyn std::error::Error + Send + Sync>>;
     async fn store_transaction_info(
+        &self,
+        tx_info: TransactionInfo,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>>;
+    async fn store_token(
+        &self,
+        nft_info: NFTInfo,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>>;
+    async fn store_contract(
+        &self,
+        contract_info: ContractInfo,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>>;
+    async fn store_token_event(
         &self,
         tx_info: TransactionInfo,
     ) -> Result<(), Box<dyn std::error::Error + Send + Sync>>;
