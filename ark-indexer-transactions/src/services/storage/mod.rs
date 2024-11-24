5d9a034d488aa351c2d99a715ff8ed8c3c6e2baa
Patrice Tisserand <p.tisserand@gmail.com>
2024-11-25 00:34:34 +0100

    ark-indexer-transactions: fix build and add `active_orders` table support

--- ark-indexer-transactions/src/services/storage/mod.rs
+++ ark-indexer-transactions/src/services/storage/mod.rs
@@ -18,6 +18,10 @@ pub trait NFTInfoStorage {
         &self,
         nft_info: NFTInfo,
     ) -> Result<(), Box<dyn std::error::Error + Send + Sync>>;
+    async fn store_token(
+        &self,
+        nft_info: NFTInfo,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>>;
 }
 
 #[async_trait]
@@ -27,18 +31,22 @@ pub trait TransactionInfoStorage {
         &self,
         tx_info: TransactionInfo,
     ) -> Result<(), Box<dyn std::error::Error + Send + Sync>>;
-    async fn store_token(
-        &self,
-        nft_info: NFTInfo,
-    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>>;
-    async fn store_contract(
-        &self,
-        contract_info: ContractInfo,
-    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>>;
     async fn store_token_event(
         &self,
         tx_info: TransactionInfo,
     ) -> Result<(), Box<dyn std::error::Error + Send + Sync>>;
 }
 
-pub trait Storage: NFTInfoStorage + TransactionInfoStorage + OrderbookStorage {}
+#[async_trait]
+#[cfg_attr(test, automock)]
+pub trait ContractInfoStorage {
+    async fn store_contract(
+        &self,
+        contract_info: ContractInfo,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>>;
+}
+
+pub trait Storage:
+    NFTInfoStorage + TransactionInfoStorage + ContractInfoStorage + OrderbookStorage
+{
+}
