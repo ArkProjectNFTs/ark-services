04938a8b80bcbbf45700f2c7cf666ad27573aa8e
remiroyc <remiroycourt@gmail.com>
2024-11-29 12:39:58 +0100

    chore: remove ark-orderbook-api

--- infrastructure/cdk-ark-orderbookapi/lib/types.ts
+++
@@ -1,7 +0,0 @@
-import * as cdk from "aws-cdk-lib";
-
-export interface ArkOrderbookApiStackProps extends cdk.StackProps {
-  networks: string[];
-  isProductionEnvironment: boolean;
-  indexerVersion: string;
-}
