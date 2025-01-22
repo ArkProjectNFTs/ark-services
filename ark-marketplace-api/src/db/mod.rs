0b5090694117605656ee8071bf8c93a63b00160e
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2025-01-22 19:12:48 +0100

    chore: remove `ark-marketplace-api` 🧹 (#688)
    * feat(ark-marketplace-api): remove rust api
    
    * fix: remove ark-marketplace-api
    
    * fix: update cargo.lock

--- ark-marketplace-api/src/db/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod db_access;
-pub mod default_db_access;
-pub mod default_query;
-pub mod portfolio_db_access;
-pub mod portfolio_query;
-pub mod query;
--- ark-marketplace-api/src/handlers/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod collection_handler;
-pub mod default_handler;
-pub mod portfolio_handler;
-pub mod token_handler;
-pub mod utils;
