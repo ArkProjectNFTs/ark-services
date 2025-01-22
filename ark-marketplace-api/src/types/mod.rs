475f7ed54b6f10680bd74444b87da3e6aa533f7b
remiroyc <remiroycourt@gmail.com>
2025-01-22 18:58:50 +0100

    feat(ark-marketplace-api): remove rust api

--- ark-marketplace-api/src/types/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod collection;
-pub mod default;
-pub mod offer_type;
-pub mod portfolio;
-pub mod token;
--- ark-marketplace-api/src/utils/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod app_config;
-pub mod currency_utils;
-pub mod db_utils;
-pub mod http_utils;
-pub mod sql_utils;
