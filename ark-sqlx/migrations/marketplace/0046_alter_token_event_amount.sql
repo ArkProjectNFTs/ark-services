522cf38070a0e37fc8b98369cdc59f12ba163c4a
Rémi Roycourt <remiroycourt@gmail.com>
2025-01-30 21:22:32 +0100

    fix(ark-sqlx): rename eth_amount to amount_eth

--- ark-sqlx/migrations/marketplace/0046_alter_token_event_amount.sql
+++ ark-sqlx/migrations/marketplace/0046_alter_token_event_amount.sql
@@ -1,2 +1,6 @@
 ALTER TABLE token_event
-ALTER COLUMN amount_eth TYPE NUMERIC;
+RENAME COLUMN eth_amount TO amount_eth;
+
+ALTER TABLE token_event 
+ALTER COLUMN amount_eth TYPE numeric USING amount_eth::numeric;
+
